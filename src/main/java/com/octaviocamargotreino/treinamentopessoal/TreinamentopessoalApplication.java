ff75b35d81348314a393dfafd26b03870027bcd4
octavio7h <octavioejc@gmail.com>
2025-11-13 12:06:40 -0300

    Configuração inicial

---
+++ src/main/java/com/octaviocamargotreino/treinamentopessoal/TreinamentopessoalApplication.java
@@ -0,0 +1,13 @@
+package com.octaviocamargotreino.treinamentopessoal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreinamentopessoalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TreinamentopessoalApplication.class, args);
+	}
+
+}
